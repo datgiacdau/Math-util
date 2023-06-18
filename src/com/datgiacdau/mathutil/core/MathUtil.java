9e04ca1750afae7f8f081b42f393f66284349064
Vu Tuan Dat <datgiacdau@gmail.com>
2023-06-18 13:05:17 +0700

    refactor the main code and green again

--- src/com/datgiacdau/mathutil/core/MathUtil.java
+++ src/com/datgiacdau/mathutil/core/MathUtil.java
@@ -22,17 +22,30 @@ public class MathUtil {
     //20 giai thừa 18 con số 0, vừa đủ kiểu long của Java
     //21 giai thừa tràn kiểu long
     //bài này qui ước tính n! từ 0 ->20
-    public static long getFactorial(int n){
-        if (n<0 || n>20)
-            throw new IllegalArgumentException("chi tu 0>20");
+//    public static long getFactorial(int n){
+//        if (n<0 || n>20)
+//            throw new IllegalArgumentException("chi tu 0>20");
+//        
+//        if (n==0 || n==1)
+//            return 1; //theo qui ước
+//        
+//        long product = 1; // tích nhân dồn
+//        for (int i = 2; i <= n; i++) 
+//            product *=i; //product = product *i;
+//        return product;
+//            
+//        }
+    //Học đệ qui - hiện tượng gọi lại chính mình vs qui mô khác
+    //Ví dụ: búp bê Nga, giống nhau và lồng trong nhau
+    // tính n! = n * (n-1)!
+        public static long getFactorial(int n){
+        if (n<0||n>20)
+            throw new IllegalArgumentException("n must be between 0 .. 20");
         
         if (n==0 || n==1)
             return 1; //theo qui ước
         
-        long product = 1; // tích nhân dồn
-        for (int i = 2; i <= n; i++) 
-            product *=i; //product = product *i;
-        return product;
-            
+        return n * getFactorial(n - 1); 
         }
+        
 }
